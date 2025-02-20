9d1e9e99eab0133c20c5744ae23e868ee379065b
Titas Antanaitis <titasantanaitis@gmail.com>
2025-02-20 19:42:32 +0200

    update main method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,28 +1,23 @@
 package com.gildedrose;
 
-import com.gildedrose.item.DecoratableItemBuilder;
 import com.gildedrose.item.ItemFactory;
-import com.gildedrose.item.LegendaryItemWrapper;
 import com.gildedrose.item.NonDecoratableItem;
 
 import java.util.Arrays;
-import java.util.List;
 
 class GildedRose {
     Item[] items;
-    private static final List<String> SPECIAL_ITEM_NAMES = Arrays.asList("Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", "Aged Brie");
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-
-        for (int i = 0; i < items.length; i++) {
-            NonDecoratableItem item = ItemFactory.getInvetoryItem(items[i]);
-            int newSellIn = item.getSellIn();
-            int newQuality = item.getQuality();
-            items[i].quality = newQuality;
-            items[i].sellIn = newSellIn;
-        }
+        Arrays.stream(items).forEach(item -> {
+            NonDecoratableItem nonDecoratableItem = ItemFactory.getInvetoryItem(item);
+            int newSellIn = nonDecoratableItem.getSellIn();
+            int newQuality = nonDecoratableItem.getQuality();
+            item.quality = newQuality;
+            item.sellIn = newSellIn;
+        });
     }
 }
