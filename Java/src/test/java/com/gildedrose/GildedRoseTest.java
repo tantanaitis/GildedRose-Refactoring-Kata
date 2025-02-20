0593de655c1c4287bd69b834a5d9bf6c94519fb1
tantanaitis <titasantanaitis@gmail.com>
2025-02-20 15:14:48 +0200

    rename, tests, add missing for sulfuras

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -87,7 +87,7 @@ class GildedRoseTest {
     }
 
     @Test
-    void sulfurSellInNotZeroSellInAndQualityDoesNotChange() {
+    void sulfurSellInNotZeroSellInAndQualityDoesNotChangeTest() {
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 80)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -97,13 +97,23 @@ class GildedRoseTest {
     }
 
     @Test
-    void sulfurSellInZeroSellInAndQualityDoesNotChange() {
+    void sulfurSellInZeroSellInAndQualityMaximumDoesNotChangeTest() {
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
         assertEquals(0, app.items[0].sellIn);
         assertEquals(80, app.items[0].quality);
+    }
+
+    @Test
+    void sulfurSellInMinusAndQualityMaximumMinusDoesNotChangeTest() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -1, -1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(-1, app.items[0].quality);
     }
 
     @Test
