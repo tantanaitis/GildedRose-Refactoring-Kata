24c392fb513907c88202ea8ab7fd796be5531f14
Titas Antanaitis <titasantanaitis@gmail.com>
2025-02-20 19:49:27 +0200

    add ConjuredItemTest

---
+++ Java/src/test/java/com/gildedrose/ConjuredItemTest.java
@@ -0,0 +1,38 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConjuredItemTest {
+    @Test
+    void sellInNotZeroQualityNotZeroSellInDecreasesBy1AndQualityDoesNotDropBelowZeroTest() {
+        Item[] items = new Item[] { new Item("conjured", 1, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("conjured", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void sellIn1Quality2SellInDecreasesBy1AndQualityDecreasesBy2Test() {
+        Item[] items = new Item[] { new Item("conjured", 1, 2) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("conjured", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void sellInZeroQualityMinus1QualityDoesNotChangeTest() {
+        Item[] items = new Item[] { new Item("conjured", 0, -1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("conjured", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(-1, app.items[0].quality);
+    }
+
+}
