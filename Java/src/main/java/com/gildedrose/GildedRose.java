9d8e5d20cfe27937b8c48df9130149f13583be96
Titas Antanaitis <titasantanaitis@gmail.com>
2025-02-20 19:35:52 +0200

    introducte ItemFactory

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,7 @@
 package com.gildedrose;
 
 import com.gildedrose.item.DecoratableItemBuilder;
+import com.gildedrose.item.ItemFactory;
 import com.gildedrose.item.LegendaryItemWrapper;
 import com.gildedrose.item.NonDecoratableItem;
 
@@ -17,44 +18,11 @@ class GildedRose {
     public void updateQuality() {
 
         for (int i = 0; i < items.length; i++) {
-            if (!SPECIAL_ITEM_NAMES.contains(items[i].name)) {
-                NonDecoratableItem item = DecoratableItemBuilder.from(items[i])
-                    .withRegularQualityDecrease()
-                    .withRegularSellInDecrease()
-                    .build();
-                items[i].sellIn = item.getSellIn();
-                items[i].quality  = item.getQuality();
-                continue;
-            }
-
-            if ("Aged Brie".equals(items[i].name)) {
-                NonDecoratableItem item = DecoratableItemBuilder.from(items[i])
-                    .withAgedBrieQualityIncrease()
-                    .withRegularSellInDecrease()
-                    .build();
-                items[i].sellIn = item.getSellIn();
-                items[i].quality  = item.getQuality();
-                continue;
-            }
-            if ("Sulfuras, Hand of Ragnaros".equals(items[i].name)) {
-                NonDecoratableItem item = new LegendaryItemWrapper(items[i]);
-                items[i].sellIn = item.getSellIn();
-                items[i].quality  = item.getQuality();
-                continue;
-            }
-            if ("Backstage passes to a TAFKAL80ETC concert".equals(items[i].name)) {
-                NonDecoratableItem item = DecoratableItemBuilder.from(items[i])
-                    .withAgedBrieQualityIncrease()
-                    .withQualityIncreaseOnSellInDateCondition(10)
-                    .withQualityIncreaseOnSellInDateCondition(5)
-                    .withQualityToZeroOnSellinDate()
-                    .withRegularSellInDecrease()
-                    .build();
-                int newSellIn = item.getSellIn();
-                int newQuality = item.getQuality();
-                items[i].quality = newQuality;
-                items[i].sellIn = newSellIn;
-            }
+            NonDecoratableItem item = ItemFactory.getInvetoryItem(items[i]);
+            int newSellIn = item.getSellIn();
+            int newQuality = item.getQuality();
+            items[i].quality = newQuality;
+            items[i].sellIn = newSellIn;
         }
     }
 }
