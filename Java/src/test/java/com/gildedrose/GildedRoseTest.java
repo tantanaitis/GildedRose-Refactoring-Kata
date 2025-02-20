cae7e177a1fddb45975385e496f73007b3f61fd0
Titas Antanaitis <titasantanaitis@gmail.com>
2025-02-20 13:22:31 +0200

    add tests for Sulfuras

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -56,4 +56,23 @@ class GildedRoseTest {
         assertEquals(50, app.items[0].quality);
     }
 
+    @Test
+    void sulfurSellInNotZeroSellInAndQualityDoesNotChange() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(1, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
+
+    @Test
+    void sulfurSellInZeroSellInAndQualityDoesNotChange() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
 }
